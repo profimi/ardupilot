bugobliterator/ardupilot-chibios

AP_BattMonitor: replace libuavcan with libcanard based driver

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
@@ -2,15 +2,10 @@
 
 #include "AP_BattMonitor.h"
 #include "AP_BattMonitor_Backend.h"
-
+#if HAL_ENABLE_LIBUAVCAN_DRIVERS
 #include <AP_UAVCAN/AP_UAVCAN.h>
-#include <mppt/OutputEnable.hpp>
 
 #define AP_BATTMONITOR_UAVCAN_TIMEOUT_MICROS         5000000 // sensor becomes unhealthy if no successful readings for 5 seconds
-
-class BattInfoCb;
-class BattInfoAuxCb;
-class MpptStreamCb;
 
 class AP_BattMonitor_UAVCAN : public AP_BattMonitor_Backend
 {
@@ -49,13 +44,13 @@ public:
 
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
     static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id);
-    static void handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb);
-    static void handle_battery_info_aux_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoAuxCb &cb);
-    static void handle_mppt_stream_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const MpptStreamCb &cb);
+    static void handle_battery_info_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const uavcan_equipment_power_BatteryInfo &msg);
+    static void handle_battery_info_aux_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const ardupilot_equipment_power_BatteryInfoAux &msg);
+    static void handle_mppt_stream_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const mppt_Stream &msg);
 
 private:
-    void handle_battery_info(const BattInfoCb &cb);
-    void handle_battery_info_aux(const BattInfoAuxCb &cb);
+    void handle_battery_info(const uavcan_equipment_power_BatteryInfo &msg);
+    void handle_battery_info_aux(const ardupilot_equipment_power_BatteryInfoAux &msg);
     void update_interim_state(const float voltage, const float current, const float temperature_K, const uint8_t soc);
 
     static bool match_battery_id(uint8_t instance, uint8_t battery_id) {
@@ -70,7 +65,7 @@ private:
         OVER_CURRENT        = (1U<<2),
         OVER_TEMPERATURE    = (1U<<3),
     };
-    void handle_mppt_stream(const MpptStreamCb &cb);
+    void handle_mppt_stream(const mppt_Stream &msg);
     void mppt_set_bootup_powered_state();
     void mppt_set_armed_powered_state();
     void mppt_set_powered_state(bool power_on, bool force);
@@ -94,7 +89,7 @@ private:
     bool _has_consumed_energy;
     bool _has_battery_info_aux;
     uint8_t _instance;                  // instance of this battery monitor
-    uavcan::Node<0> *_node;             // UAVCAN node id
+
     AP_Float _curr_mult;                 // scaling multiplier applied to current reports for adjustment
     // MPPT variables
     struct {
@@ -104,4 +99,10 @@ private:
         bool vehicle_armed_last;        // latest vehicle armed state. used to detect changes and power on/off MPPT board
         uint8_t fault_flags;            // bits holding fault flags
     } _mppt;
+
+    void handle_mppt_output_enable(const CanardRxTransfer&, const mppt_OutputEnableResponse&) {}
+
+    Canard::ObjCallback<AP_BattMonitor_UAVCAN, mppt_OutputEnableResponse> mppt_outputenable_res_cb{this, &AP_BattMonitor_UAVCAN::handle_mppt_output_enable};
+    Canard::Client<mppt_OutputEnable_cxx_iface> *mppt_outputenable_client;
 };
+#endif

profimi/ardupilot

libraries/AP_BattMonitor/AP_BattMonitor_UAVCAN.h
@@ -2,19 +2,14 @@
 
 #include "AP_BattMonitor.h"
 #include "AP_BattMonitor_Backend.h"
-
+#if HAL_ENABLE_LIBUAVCAN_DRIVERS
 #include <AP_UAVCAN/AP_UAVCAN.h>
-#include <mppt/OutputEnable.hpp>
 
 #define AP_BATTMONITOR_UAVCAN_TIMEOUT_MICROS         5000000 // sensor becomes unhealthy if no successful readings for 5 seconds
 
 #ifndef AP_BATTMONITOR_UAVCAN_MPPT_DEBUG
 #define AP_BATTMONITOR_UAVCAN_MPPT_DEBUG 0
 #endif
-
-class BattInfoCb;
-class BattInfoAuxCb;
-class MpptStreamCb;
 
 class AP_BattMonitor_UAVCAN : public AP_BattMonitor_Backend
 {
@@ -53,16 +48,15 @@ public:
 
     static void subscribe_msgs(AP_UAVCAN* ap_uavcan);
     static AP_BattMonitor_UAVCAN* get_uavcan_backend(AP_UAVCAN* ap_uavcan, uint8_t node_id, uint8_t battery_id);
-    static void handle_battery_info_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoCb &cb);
-    static void handle_battery_info_aux_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const BattInfoAuxCb &cb);
-    static void handle_mppt_stream_trampoline(AP_UAVCAN* ap_uavcan, uint8_t node_id, const MpptStreamCb &cb);
-    void handle_outputEnable_response(const uint8_t nodeId, const bool enabled);
+    static void handle_battery_info_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const uavcan_equipment_power_BatteryInfo &msg);
+    static void handle_battery_info_aux_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const ardupilot_equipment_power_BatteryInfoAux &msg);
+    static void handle_mppt_stream_trampoline(AP_UAVCAN *ap_uavcan, const CanardRxTransfer& transfer, const mppt_Stream &msg);
 
     void mppt_set_powered_state(bool power_on) override;
 
 private:
-    void handle_battery_info(const BattInfoCb &cb);
-    void handle_battery_info_aux(const BattInfoAuxCb &cb);
+    void handle_battery_info(const uavcan_equipment_power_BatteryInfo &msg);
+    void handle_battery_info_aux(const ardupilot_equipment_power_BatteryInfoAux &msg);
     void update_interim_state(const float voltage, const float current, const float temperature_K, const uint8_t soc);
 
     static bool match_battery_id(uint8_t instance, uint8_t battery_id) {
@@ -77,7 +71,7 @@ private:
         OVER_CURRENT        = (1U<<2),
         OVER_TEMPERATURE    = (1U<<3),
     };
-    void handle_mppt_stream(const MpptStreamCb &cb);
+    void handle_mppt_stream(const mppt_Stream &msg);
     void mppt_check_powered_state();
 
 #if AP_BATTMONITOR_UAVCAN_MPPT_DEBUG
@@ -102,7 +96,7 @@ private:
     bool _has_consumed_energy;
     bool _has_battery_info_aux;
     uint8_t _instance;                  // instance of this battery monitor
-    uavcan::Node<0> *_node;             // UAVCAN node id
+
     AP_Float _curr_mult;                 // scaling multiplier applied to current reports for adjustment
     // MPPT variables
     struct {
@@ -112,4 +106,10 @@ private:
         uint8_t fault_flags;            // bits holding fault flags
         uint32_t powered_state_remote_ms; // timestamp of when request was sent, zeroed on response. Used to retry
     } _mppt;
+
+    void handle_outputEnable_response(const CanardRxTransfer&, const mppt_OutputEnableResponse&);
+
+    Canard::ObjCallback<AP_BattMonitor_UAVCAN, mppt_OutputEnableResponse> mppt_outputenable_res_cb{this, &AP_BattMonitor_UAVCAN::handle_outputEnable_response};
+    Canard::Client<mppt_OutputEnableResponse> *mppt_outputenable_client;
 };
+#endif